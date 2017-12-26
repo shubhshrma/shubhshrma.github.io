481bd274cb18349784e64df5c373b7dc1b5784be
shubham <shubham.31996@gmail.com>
2017-12-27 02:02:47 +0530

    update

---
+++ script.js
@@ -0,0 +1,11 @@
+function toPomodoro(){
+	window.location = "https://codepen.io/shubhshrma/full/Evxppd/";
+}
+
+function toTwitch(){
+	window.location = "https://codepen.io/shubhshrma/full/YQemgy/";
+}
+
+function toCalculator(){
+	window.location = "https://codepen.io/shubhshrma/full/jwdNjN/";
+}
